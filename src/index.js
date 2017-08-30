a6ce0bf896a5c85a0d7cc6077cbd1098008a0d74
Brian Bergendahl <bbergendahl@backcountry.com>
2017-08-30 00:05:34 -0600

    Following UMD standard to support both AMD and CommonJS (see: https://github.com/umdjs/umd)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
-(function ($) {
+//UMD pattern from: https://github.com/umdjs/umd/blob/master/templates/jqueryPlugin.js
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
   $.fn.ajaxTracking = function(opts) {
     if (!opts.callback || typeof opts.callback !== 'function') {
       throw Error('Ajax tracking requires a valid callback function');
@@ -15,4 +42,4 @@
       };
     });
   }
-}(jQuery));
+}));
